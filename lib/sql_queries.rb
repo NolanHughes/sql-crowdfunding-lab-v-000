355efb1284634860dc9e648bffe258411edc1383
Nolan Hughes <nhughes987@gmail.com>
2018-01-07 18:37:33 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -34,12 +34,12 @@ HAVING SUM(Pledges.amount) >= Projects.funding_goal;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-  "SELECT Users.name, Users.age, SUM(Pledges.amount)
+  "SELECT Users.name, SUM(Pledges.amount)
   FROM pledges
   LEFT OUTER JOIN users
   ON pledges.user_id = users.id
   GROUP BY user_id
-  ORDER BY(Users.name);"
+  ORDER BY(SUM(Pledges.amount));"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
